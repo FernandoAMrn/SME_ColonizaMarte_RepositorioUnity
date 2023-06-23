67e0073b1e3a416564c8d74c09b919f09c985430
Fernando Moreno Arroyo <76887786+FernandoAMrn@users.noreply.github.com>
2023-06-23 09:40:55 -0600

    Nuevos Modelos + Camara
    Importacion de los modelos finales para el juego. Junto con placeholders para UI. Al igual que mejoras al sistema de control de camara.

--- SME_ColonizaMarte/Assets/C#Scripts/InputSystem.cs
+++ SME_ColonizaMarte/Assets/C#Scripts/InputSystem.cs
@@ -4,14 +4,23 @@ using UnityEngine;
 
 public class InputSystem : MonoBehaviour
 {
+    public Transform cameraTransform;
+
     public float cameraMovementSpeed;
     public float cameraMovementTime;
 
-    public Vector3 newPosition;
+    // Velocidades para el movimiento de la camara. 
+    public float normalSpeed; 
+    public float fastSpeed;
+
+    public Vector3 newPosition; // Offset para el rig de la camara
+    public Vector3 zoomAmount;
+    public Vector3 newZoom;
 
     private void Start()
     {
         newPosition = transform.position; //Mantiene la posicion de la camara cuando se empieza el juego
+        newZoom = cameraTransform.localPosition;
     }
 
     private void Update()
@@ -21,6 +30,16 @@ public class InputSystem : MonoBehaviour
 
     void CameraMovementInput()  // Controles para la Camara
     {
+        // Velocidades de movimiento de la camara
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            cameraMovementSpeed = fastSpeed;
+        }
+        else
+        {
+            cameraMovementSpeed = normalSpeed;
+        }
+        // Movimiento de la camara
         if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
         {
             newPosition += (transform.forward * cameraMovementSpeed);
@@ -37,7 +56,18 @@ public class InputSystem : MonoBehaviour
         {
             newPosition += (transform.right * -cameraMovementSpeed);
         }
-
-        transform.position = Vector3.Lerp(transform.position, newPosition, Time.deltaTime * cameraMovementTime); //Movimiento suave de la camara
+        //Movimiento suave de la camara
+        transform.position = Vector3.Lerp(transform.position, newPosition, Time.deltaTime * cameraMovementTime);
+        cameraTransform.localPosition = Vector3.Lerp(cameraTransform.localPosition, newZoom, Time.deltaTime * cameraMovementTime);
+        //Zoom de la camara
+        if (Input.GetKey(KeyCode.R))
+        {
+            newZoom += zoomAmount;
+        }
+        if (Input.GetKey(KeyCode.F))
+        {
+            newZoom -= zoomAmount;
+        }
+        
     }
 }
