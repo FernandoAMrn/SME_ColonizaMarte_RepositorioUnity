de34ab09b7043d896a60959a556fd154c45b4efb
Fernando Moreno Arroyo <76887786+FernandoAMrn@users.noreply.github.com>
2023-06-30 09:30:37 -0600

    UI Escalable + Scripts para construccion
    UI Escalable a diferentes resoluciones de pantalla y scripts para construccion de edificios.

---
+++ SME_ColonizaMarte/Assets/C#Scripts/Tile.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile 
+{
+    public Building buildingRef;
+    public bool occupied;
+
+    public ObstacleType obstacleType;
+    public enum ObstacleType
+    {
+        None,
+        Building
+    }
+
+    public void SetOccupied(ObstacleType t)
+    {
+        occupied = true;
+        obstacleType = t;
+    }
+
+    public void SetOccupied(ObstacleType t, Building b)
+    {
+        occupied = true;
+        obstacleType = t;
+        buildingRef = b;
+    }
+    public void CleanTile()
+    {
+        obstacleType = ObstacleType.None;
+        occupied = false;
+    }
+}
