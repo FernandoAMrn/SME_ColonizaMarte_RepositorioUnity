11b95df4077f7a0fd7ebd02f975869571a5ab3ad
Fernando Moreno Arroyo <76887786+FernandoAMrn@users.noreply.github.com>
2023-07-02 19:42:08 -0600

    Inventorio de recursos

---
+++ SME_ColonizaMarte/Assets/C#Scripts/ResourceManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceManager : MonoBehaviour
+{
+    [Header("Resources")]
+
+    [Space(10)]
+
+    public int maxPeople;
+    int people = 0;
+
+    public int maxFood;
+    int food = 0;
+
+    public int maxEnergy;
+    int energy = 0;
+    
+    
+
+}
