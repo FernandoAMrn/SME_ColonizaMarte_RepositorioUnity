34057fbf1e6e393ac0eca30fbde7beda863d33f1
Fernando Moreno Arroyo <76887786+FernandoAMrn@users.noreply.github.com>
2023-08-10 22:49:16 -0600

    Sistema de construccion con diferentes edificios
    Sistema de construccion completo falta hacer que los edifcios tengan un costo de materiales

--- SME_ColonizaMarte/Assets/C#Scripts/BUILDING SCRIPTS/PlacementSystem.cs
+++ SME_ColonizaMarte/Assets/C#Scripts/BUILDING SCRIPTS/PlacementSystem.cs
@@ -12,27 +12,95 @@ public class PlacementSystem : MonoBehaviour
     [SerializeField]
     private Grid grid;
 
+    //GameObjects de prefabs de edificios
     [SerializeField]
     private GameObject dormitorios;
+    [SerializeField]
+    private GameObject EstacionDeAgua;
+    [SerializeField]
+    private GameObject EstacionDeEnergia;
+    [SerializeField]
+    private GameObject Invernadero;
+    [SerializeField]
+    private GameObject Laboratorio;
+    [SerializeField]
+    private GameObject Taller;
 
     [SerializeField]
     private GameObject gridVisualization;
 
     private void Start()
     {
-        StopPlacement();
+        StopPlacementDormitorio();
+        StopPlacementAgua();
+        StopPlacementEnergia();
+        StopPlacementInverandero();
+        StopPlacementLaboratorio();
+        StopPlacementTaller();
     }
 
-    public void StartPlacement()
+    //Modos de Construccion
+    #region
+    public void StartPlacementDormitorio()
     {
-        StopPlacement();
+        StopPlacementDormitorio();
         gridVisualization.SetActive(true);
         cellIndicator.SetActive(true);
-        inputManager.OnClicked += PlaceStructure;
-        inputManager.OnExit += StopPlacement;
+        inputManager.OnClicked += PlaceStructureDormitorios;
+        inputManager.OnExit += StopPlacementDormitorio;
+    }
+    public void StartPlacementAgua()
+    {
+        StopPlacementAgua();
+        gridVisualization.SetActive(true);
+        cellIndicator.SetActive(true);
+        inputManager.OnClicked += PlaceStructureAgua;
+        inputManager.OnExit += StopPlacementAgua;
     }
 
-    private void PlaceStructure()
+    public void StartPlacementEnergia()
+    {
+        StopPlacementEnergia();
+        gridVisualization.SetActive(true);
+        cellIndicator.SetActive(true);
+        inputManager.OnClicked += PlaceStructureEnergia;
+        inputManager.OnExit += StopPlacementEnergia;
+    }
+
+    public void StartPlacementInvernadero()
+    {
+        StopPlacementInverandero();
+        gridVisualization.SetActive(true);
+        cellIndicator.SetActive(true);
+        inputManager.OnClicked += PlaceStructureInvernadero;
+        inputManager.OnExit += StopPlacementInverandero;
+    }
+    public void StartPlacementLaboratorio()
+    {
+        StopPlacementLaboratorio();
+        gridVisualization.SetActive(true);
+        cellIndicator.SetActive(true);
+        
+        inputManager.OnClicked += PlaceStructureLaboratorio;
+        
+        inputManager.OnExit += StopPlacementLaboratorio;
+    }
+
+    public void StartPlacementTaller()
+    {
+        StopPlacementTaller();
+        gridVisualization.SetActive(true);
+        cellIndicator.SetActive(true);
+        
+        inputManager.OnClicked += PlaceStructureTaller;
+        inputManager.OnExit += StopPlacementTaller;
+    }
+    #endregion
+    //Termina modos de Construccion
+
+    //Instancias de estructuras
+    #region
+    private void PlaceStructureDormitorios()
     {
         if(inputManager.IspointerOverUI())
         {
@@ -45,14 +113,134 @@ public class PlacementSystem : MonoBehaviour
         newDormitorios.transform.position = grid.CellToWorld(gridPosition);
     }
 
-    private void StopPlacement()
+    private void PlaceStructureAgua()
+    {
+        if (inputManager.IspointerOverUI())
+        {
+            return;
+        }
+
+        Vector3 mousePosition = inputManager.GetSelectedMapPosition();
+        Vector3Int gridPosition = grid.WorldToCell(mousePosition);
+        GameObject newDormitorios = Instantiate(EstacionDeAgua);
+        newDormitorios.transform.position = grid.CellToWorld(gridPosition);
+    }
+
+    private void PlaceStructureEnergia()
+    {
+        if (inputManager.IspointerOverUI())
+        {
+            return;
+        }
+
+        Vector3 mousePosition = inputManager.GetSelectedMapPosition();
+        Vector3Int gridPosition = grid.WorldToCell(mousePosition);
+        GameObject newDormitorios = Instantiate(EstacionDeEnergia);
+        newDormitorios.transform.position = grid.CellToWorld(gridPosition);
+    }
+
+    private void PlaceStructureInvernadero()
+    {
+        if (inputManager.IspointerOverUI())
+        {
+            return;
+        }
+
+        Vector3 mousePosition = inputManager.GetSelectedMapPosition();
+        Vector3Int gridPosition = grid.WorldToCell(mousePosition);
+        GameObject newDormitorios = Instantiate(Invernadero);
+        newDormitorios.transform.position = grid.CellToWorld(gridPosition);
+    }
+
+    private void PlaceStructureLaboratorio()
+    {
+        if (inputManager.IspointerOverUI())
+        {
+            return;
+        }
+
+        Vector3 mousePosition = inputManager.GetSelectedMapPosition();
+        Vector3Int gridPosition = grid.WorldToCell(mousePosition);
+        GameObject newDormitorios = Instantiate(Laboratorio);
+        newDormitorios.transform.position = grid.CellToWorld(gridPosition);
+    }
+
+    private void PlaceStructureTaller()
+    {
+        if (inputManager.IspointerOverUI())
+        {
+            return;
+        }
+
+        Vector3 mousePosition = inputManager.GetSelectedMapPosition();
+        Vector3Int gridPosition = grid.WorldToCell(mousePosition);
+        GameObject newDormitorios = Instantiate(Taller);
+        newDormitorios.transform.position = grid.CellToWorld(gridPosition);
+    }
+
+
+
+    #endregion
+
+    //Termina Instancias de Estructuras
+
+    //Exit modo de construccion
+    #region
+    private void StopPlacementDormitorio()
     {
         gridVisualization.SetActive(false);
         cellIndicator.SetActive(false);
-        inputManager.OnClicked -= PlaceStructure;
-        inputManager.OnExit -= StopPlacement;
+        inputManager.OnClicked -= PlaceStructureDormitorios;
+        
+        inputManager.OnExit -= StopPlacementDormitorio;
     }
 
+    private void StopPlacementAgua()
+    {
+        gridVisualization.SetActive(false);
+        cellIndicator.SetActive(false);
+        inputManager.OnClicked -= PlaceStructureAgua;
+        inputManager.OnExit -= StopPlacementAgua;
+    }
+
+    private void StopPlacementEnergia()
+    {
+        gridVisualization.SetActive(false);
+        cellIndicator.SetActive(false);
+        inputManager.OnClicked -= PlaceStructureEnergia;
+        inputManager.OnExit -= StopPlacementEnergia;
+    }
+
+    private void StopPlacementInverandero()
+    {
+        gridVisualization.SetActive(false);
+        cellIndicator.SetActive(false);
+        inputManager.OnClicked -= PlaceStructureInvernadero;
+        inputManager.OnExit -= StopPlacementInverandero;
+    }
+
+    private void StopPlacementLaboratorio()
+    {
+        gridVisualization.SetActive(false);
+        cellIndicator.SetActive(false);
+        
+        inputManager.OnClicked -= PlaceStructureLaboratorio;
+        
+        inputManager.OnExit -= StopPlacementLaboratorio;
+    }
+
+    private void StopPlacementTaller()
+    {
+        gridVisualization.SetActive(false);
+        cellIndicator.SetActive(false);
+        inputManager.OnClicked -= PlaceStructureTaller;
+        inputManager.OnExit -= StopPlacementTaller;
+    }
+#endregion
+    //Termina Exit modos de construccion
+
+
+    //UPDATE
     private void Update()
     {
        
