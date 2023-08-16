3516bb949f5be0e6a8cb5142d712fbdc91ea1a94
Fernando Moreno Arroyo <76887786+FernandoAMrn@users.noreply.github.com>
2023-08-15 20:14:12 -0600

    Sisteama de tiempo de construccion.
    Bases del sistema de construccion.

--- SME_ColonizaMarte/Assets/C#Scripts/BUILDING SCRIPTS/Dormitorios.cs
+++ SME_ColonizaMarte/Assets/C#Scripts/BUILDING SCRIPTS/Dormitorios.cs
@@ -1,14 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using CodeMonkey.Utils;
 
 public class Dormitorios : MonoBehaviour
 {
+    
+    private World_Bar constructingWorldBar;
     // Start is called before the first frame update
     private void Start()
     {
         StartCoroutine(PeopleGen());
+        constructingWorldBar = new World_Bar(gameObject.transform, new Vector3(0, 1f), new Vector3(2, 1), Color.grey, Color.yellow, .0f, -10, new World_Bar.Outline() { color = Color.black, size = .01f });
     }
+
+    
 
     IEnumerator PeopleGen()
     {
